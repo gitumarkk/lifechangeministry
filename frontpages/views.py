387da61095bb756392ec64ecb929dc1d952386fb
gitumarkk <gitumarkk@gmail.com>
2014-01-29 16:22:33 +0200

    created basic page functions in views

--- frontpages/views.py
+++ frontpages/views.py
@@ -1,3 +1,27 @@
-from django.shortcuts import render
-
 # Create your views here.
+from django.shortcuts import render, redirect
+from django.core.context_processors import csrf
+from django.core.urlresolvers import reverse
+from django.template import RequestContext
+from django.contrib import messages
+
+
+def home(request):
+    context = {}
+    return render(request,
+                  "frontpages/home.html",
+                  context)
+
+
+def about(request):
+    context = {}
+    return render(request,
+                  "frontpages/about.html",
+                  context)
+
+
+def contact(request):
+    context = {}
+    return render(request,
+                  "frontpages/contact.html",
+                  context)
