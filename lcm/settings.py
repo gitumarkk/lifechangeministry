6d49a65df1c84e75d2ddb08b938fcdae2213d98a
gitumarkk <gitumarkk@gmail.com>
2014-01-29 16:31:35 +0200

    Updated static files to find templates

--- lcm/settings.py
+++ lcm/settings.py
@@ -54,6 +54,9 @@ MIDDLEWARE_CLASSES = (
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 )
 
+# ========================================================
+# TEMPLATES CONFIGURATION
+# ========================================================
 TEMPLATE_CONTEXT_PROCESSORS = (
     "django.contrib.auth.context_processors.auth",
     "django.core.context_processors.i18n",
@@ -61,6 +64,16 @@ TEMPLATE_CONTEXT_PROCESSORS = (
     "django.core.context_processors.static",
     "django.core.context_processors.tz",
     "django.contrib.messages.context_processors.messages")
+
+TEMPLATE_DIRS = (
+    # Put strings here, like "/home/html/django_templates" or "C:/www/django/templates".
+    # Always use forward slashes, even on Windows.
+    # Don't forget to use absolute paths, not relative paths.
+    os.path.join(BASE_DIR, "lcm", "templates"),
+)
+
+# END OF TEMPLATES CONFIGURATION
+# ========================================================
 
 ROOT_URLCONF = 'lcm.urls'
 
@@ -102,8 +115,18 @@ USE_TZ = True
 STATIC_URL = '/static/'
 
 STATICFILES_DIRS = (
-    os.path.join(BASE_DIR, "static"),
+    os.path.join(BASE_DIR, "lcm", "static"),
 )
+
+# Absolute path to the directory static files should be collected to.
+# Don't put anything in this directory yourself; store your static files
+# in apps' "static/" subdirectories and in STATICFILES_DIRS.
+# Example: "/var/www/example.com/static/"
+STATIC_ROOT = ''
+
+# END OF STATIC FILES CONFIGURATION
+# ========================================================
+
 
 MEDIA_URL = '/media/'
 # ========================================================
