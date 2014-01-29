b4d64a2b3da738c720c4a4836248677d714014c5
gitumarkk <gitumarkk@gmail.com>
2014-01-29 21:41:14 +0200

    added the contact model

--- frontpages/models.py
+++ frontpages/models.py
@@ -1,3 +1,15 @@
 from django.db import models
 
-# Create your models here.
+
+class Contact(models.Model):
+    email = models.EmailField(max_length=200)
+    name = models.CharField(max_length=200, blank=True)
+    message = models.TextField()
+    sent = models.BooleanField(default=False)
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    class Meta:
+        verbose_name = u'Contact Us'
+        verbose_name_plural = u'Contact Us'
+    def __unicode__(self):
+        return self.email
