a7fdae933c11c143f6afb2cb35aad509b38b6cf0
gitumarkk <gitumarkk@gmail.com>
2014-02-01 22:52:01 +0200

    adde stories, partners & donations

--- lcm/context_processors.py
+++ lcm/context_processors.py
@@ -3,9 +3,20 @@ from django.core.urlresolvers import reverse
 
 
 def highlight_active_menu(request):
-    if reverse("about") == request.path:
+    if reverse("about") in request.path:
         return {'about': True}
-    elif reverse("contact") == request.path:
+
+    elif reverse("stories") in request.path:
+        return {'stories': True}
+
+    elif reverse("partners") in request.path:
+        return {'partners': True}
+
+    elif reverse("donations") in request.path:
+        return {'donations': True}
+
+    elif reverse("contact") in request.path:
         return {'contact': True}
-    elif reverse("home") == request.path:
+
+    elif reverse("home") in request.path:
         return {"home": True}
