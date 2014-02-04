07ed65dd91e68dc61a0bb91f17200383ba2dd030
gitumarkk <gitumarkk@gmail.com>
2014-02-04 12:40:27 +0200

    updating the database variable

--- lcm/productions_settings.py
+++ lcm/productions_settings.py
@@ -1,4 +1,4 @@
 # Parse database configuration from $DATABASE_URL
 import dj_database_url
-DATABASES['default'] =  dj_database_url.config()
+DATABASES = {'default': dj_database_url.config(default='postgres://localhost')}
 print "reading database"
--- lcm/settings.py
+++ lcm/settings.py
@@ -84,13 +84,13 @@ WSGI_APPLICATION = 'lcm.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/1.6/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': 'lcm',
-        'USER': 'postgres'
-    }
-}
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.postgresql_psycopg2',
+#         'NAME': 'lcm',
+#         'USER': 'postgres'
+#     }
+# }
 
 # Internationalization
 # https://docs.djangoproject.com/en/1.6/topics/i18n/
