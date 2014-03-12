ab53733c462352740f641f49780ca134b4872cb6
gitumarkk <gitumarkk@gmail.com>
2014-03-12 22:19:32 +0200

    changed production settings

--- lcm/production_settings.py
+++ lcm/production_settings.py
@@ -1,4 +1,12 @@
-# Parse database configuration from $DATABASE_URL
-import dj_database_url
-DATABASES = {'default': dj_database_url.config(default='postgres://localhost')}
-print "reading database"
+DATABASES = {
+    'default': {
+                'ENGINE': 'django.db.backends.postgresql_psycopg2',
+                'NAME': 'lcm',
+                'USER': 'postgres',
+                'PASSWORD': 'postgres',
+                'HOST': 'localhost',
+                'PORT': '5432',
+            }
+}
+
+MANDRILL_KEY = '5432'
