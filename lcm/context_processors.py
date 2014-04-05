35dfb0489dc3b33c3f30d7fe0a21e9c395952ad0
gitumarkk <gitumarkk@gmail.com>
2014-04-05 13:56:06 +0200

    made styling changes

--- lcm/context_processors.py
+++ lcm/context_processors.py
@@ -18,8 +18,8 @@ def highlight_active_menu(request):
     elif reverse("contact") in request.path:
         return {'contact': True}
 
+    elif "events" in request.path:
+        return {"events_nav": True}
+
     elif reverse("home") in request.path:
         return {"home": True}
-
-    else:
-        return
--- lcm/static/js/lcm.js
+++ lcm/static/js/lcm.js
@@ -5,5 +5,7 @@ $(document).foundation();
 // Replacing all spans with arial with calibri
 
 $(document).ready(function() {
-  var $span = $(".tiny-mce").find("span").css({"font-family":"calibri"});
+  var $tiny_mce = $(".tiny-mce");
+  var $span = $tiny_mce.find("span");
+  $span.css({"font-family":"calibri", "font-size": "16px", "line-height": "150%"});
 });
