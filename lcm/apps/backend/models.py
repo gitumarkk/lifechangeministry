bb902935cdb69a7fdc9aaf6016efa7482cbbe2ba
gitumarkk <gitumarkk@gmail.com>
2014-08-12 22:35:19 +0200

    added recurring events

--- lcm/apps/backend/models.py
+++ lcm/apps/backend/models.py
@@ -3,8 +3,9 @@ from datetime import datetime
 import os, time
 
 # Django
+from django.template.defaultfilters import date
 from django.db import models
-
+from django.db.models import Q
 
 # Third Party
 from imagekit.processors import ResizeToFit
@@ -25,19 +26,28 @@ class Event(models.Model):
                                     format='JPEG',
                                     options={'quality': 60})
     description = models.TextField(null=True, blank=True)
+    recurring = models.BooleanField(default=False)
     event_date = models.DateTimeField('Event Date')
     created_at = models.DateTimeField(auto_now_add=True)
 
     def __unicode__(self):
         return self.title
 
+    def get_event_date(self):
+        if self.recurring:
+            return "%s happens every %s" % (self.title, date(self.event_date, "l"))
+        else:
+            return "%s is happening on %s" % (self.title, date(self.event_date, "j F o"))
+
     @classmethod
     def get_latest_event(cls):
-        return cls.objects.filter(event_date__gte=datetime.now()).order_by("event_date").first()
+        return cls.objects.filter(Q(event_date__gte=datetime.now()) |
+                                  Q(recurring=True)).order_by("event_date").first()
 
     @classmethod
     def get_events(cls):
-        return cls.objects.filter(event_date__gte=datetime.now()).order_by("event_date").all()
+        return cls.objects.filter(Q(event_date__gte=datetime.now()) |
+                                  Q(recurring=True)).order_by("event_date").all()
 
 
 class Story(models.Model):
